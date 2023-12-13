88efee0f2e77fb376d2f0ee70637f733a9e97cc3
vignesh.m <vigneshcegharur@gmail.com>
2023-12-13 18:43:08 +0530

    advanced-ds

--- python-udemy/src/main/com/learning/udemy/advanced_ds/advanced_set.py
+++ python-udemy/src/main/com/learning/udemy/advanced_ds/advanced_set.py
@@ -21,17 +21,18 @@ sc.discard(4)
 sc.discard(2)
 
 print(sc)
-
+print("----------------")
 s = {1, 2, 3}
 s1 = {1, 2, 5}
 print(s.intersection(s1))
 s.intersection_update(s1)
 print(s)
-
+print("----------------")
 s2 = {7, 6}
 print(s1.isdisjoint(s2)) # is intersection not there
 print(s.isdisjoint(s1))
 
+print("----------------")
 print(sc.issubset(s))
 print(sc.issuperset(s))
 s = {1, 2, 3}
