26140fdd6470bbd890676c06bb02c158cac54cf7
Vignesh <vigneshcegharur@gmail.com>
2023-12-19 13:47:38 +0530

    nn

---
+++ flask-udemy/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 400.0
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
